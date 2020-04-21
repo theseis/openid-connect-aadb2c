19ae8a7e66c4e60df5f4db896376378a46e88d38
SYLVAIN PAILLASSE <sylvain.paillasse@theseis.fr>
2020-04-22 00:08:35 +0200

    Disable button on login form for admins.
    Disable autologin for admins and enable it for "my account" and "order" Woocommerce pages.

--- includes/openid-connect-generic-login-form.php
+++ includes/openid-connect-generic-login-form.php
@@ -27,7 +27,7 @@ class OpenID_Connect_Generic_Login_Form {
 		$login_form = new self( $settings, $client_wrapper );
 
 		// alter the login form as dictated by settings
-		add_filter( 'login_message', array( $login_form, 'handle_login_page' ), 99 );
+		//add_filter( 'login_message', array( $login_form, 'handle_login_page' ), 99 ); // Disable button on login form
 
 		// add a shortcode for the login button
 		add_shortcode( 'openid_connect_generic_login_button', array( $login_form, 'make_login_button' ) );
@@ -42,7 +42,8 @@ class OpenID_Connect_Generic_Login_Form {
 	 */
 	function handle_redirect_login_type_auto()
 	{
-		if ( $GLOBALS['pagenow'] == 'wp-login.php'
+		if ( //$GLOBALS['pagenow'] == 'wp-login.php'
+			(($_SERVER["REQUEST_URI"] == '/mon-compte/' || $_SERVER["REQUEST_URI"] == '/commande/') && !is_user_logged_in())
 			&& ( $this->settings->login_type == 'auto' || ! empty( $_GET['force_redirect'] ) )
 			&& ( ! isset( $_GET[ 'action' ] ) || $_GET[ 'action' ] !== 'logout' )
 			&& ! isset( $_POST['wp-submit'] ) )
